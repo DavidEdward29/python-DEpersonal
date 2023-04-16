191f220b5515c91bb6ada69c511684470257a967
David Edward <edward_david29@outlook.com>
2023-04-16 14:30:43 +0100

    Initial commit

---
+++ hellotest.py
@@ -0,0 +1,13 @@
+# os function to allow clear command on Gitbash
+import os
+os.system ('clear')
+
+first_name = "Mick"
+print (first_name)
+
+first_name = "Joe"
+print (first_name)
+
+# to print hello world to the screen
+print ("hello world!")
+
